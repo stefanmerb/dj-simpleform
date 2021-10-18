94c0c1e45d9b6f328cc4c5a6c2063b98dea6ce28
stefanmerb <Stefan_Merbele@web.de>
2021-10-18 21:47:16 +0200

    adjusted css directory

--- fromTest/urls.py
+++ fromTest/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from simpleForm import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     #path('', views.index, name = 'index'),
     path('admin/', admin.site.urls),
     #path('yourname/', views.get_name, name='forms'),
     path('', views.get_name, name='forms'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
